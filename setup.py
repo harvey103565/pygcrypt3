ac71049431d5cbf458bb6e0ec780d1198b80a3a5
harvey103565 <harvey103565@gmail.com>
2023-11-21 00:11:38 +0800

    Initial devel

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+
+"""
+
+Libraries have been installed in:
+   /usr/local/lib
+
+If you ever happen to want to link against installed libraries
+in a given directory, LIBDIR, you must either use libtool, and
+specify the full pathname of the library, or use the `-LLIBDIR'
+flag during linking and do at least one of the following:
+   - add LIBDIR to the `LD_LIBRARY_PATH' environment variable
+     during execution
+   - add LIBDIR to the `LD_RUN_PATH' environment variable
+     during linking
+   - use the `-Wl,-rpath -Wl,LIBDIR' linker flag
+   - have your system administrator add LIBDIR to `/etc/ld.so.conf'
+
+See any operating system documentation about shared libraries for
+more information, such as the ld(1) and ld.so(8) manual pages.
+
+"""
+
+
+py_x_sources =  [
+    "./src/pygcrypt3/core.pyx"
+]
+
+c_include_dirs = ["./includes"]
+c_libraries = ["gcrypt"]
+c_library_dirs =["./c_libs"]
+
+extension = []
+
+setup(ext_modules=cythonize([
+        Extension("pygcrypt", 
+                  py_x_sources, 
+                  include_dirs=c_include_dirs, 
+                  libraries=c_libraries, 
+                  library_dirs=c_library_dirs)])
+)
